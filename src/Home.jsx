594a7952ac3a604aeb20ddde598ec9ddabf40c98
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-04-06 12:19:34 +0500

    UI update
    i updated UI/UX and buttons.

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,6 +10,40 @@ export default function Todo() {
             <h1 className="">
               Calculator <CalculateIcon />
             </h1>
+            <form action="">
+              <div>
+                <input className="cal-input" type="text" />
+              </div>
+              <div className="calc-btns">
+                <input className="custom-btn" type="button" value="AC" />
+                <input className="custom-btn" type="button" value="DE" />
+                <input className="custom-btn" type="button" value="." />
+                <input className="custom-btn" type="button" value="/" />
+              </div>
+              <div className="calc-btns">
+                <input className="custom-btn" type="button" value="7" />
+                <input className="custom-btn" type="button" value="8" />
+                <input className="custom-btn" type="button" value="9" />
+                <input className="custom-btn" type="button" value="*" />
+              </div>
+              <div className="calc-btns">
+                <input className="custom-btn" type="button" value="4" />
+                <input className="custom-btn" type="button" value="5" />
+                <input className="custom-btn" type="button" value="6" />
+                <input className="custom-btn" type="button" value="+" />
+              </div>
+              <div className="calc-btns">
+                <input className="custom-btn" type="button" value="1" />
+                <input className="custom-btn" type="button" value="2" />
+                <input className="custom-btn" type="button" value="3" />
+                <input className="custom-btn" type="button" value="-" />
+              </div>
+              <div className="calc-btns">
+                <input className="custom-btn" type="button" value="00" />
+                <input className="custom-btn" type="button" value="0" />
+                <input className="equals-btn" type="button" value="=" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
